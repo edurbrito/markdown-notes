9b161c2e891968d57e1ee3ebd3cf55fe4df43c8d
Eduardo Brito <developer.edurbrito@gmail.com>
2020-07-04 12:38:33 +0100

    ESLint AutoFix

--- md-notes.js
+++ md-notes.js
@@ -1,74 +1,70 @@
 #!/usr/bin/env node
 
-var inquirer = require("inquirer");
-var fs = require("fs");
+var inquirer = require('inquirer')
+var fs = require('fs')
+var path = require('path')
 
 var questions = [
   {
-    type: "list",
-    name: "type",
-    message: "What type of note?",
-    choices: ["Note", "Q&A"],
+    type: 'list',
+    name: 'type',
+    message: 'What type of note?',
+    choices: ['Note', 'Q&A'],
     filter: function (val) {
-      return val.toLowerCase().replace(/\s/g, "").trim();
-    },
+      return val.toLowerCase().replace(/\s/g, '').trim()
+    }
   },
   {
-    type: "list",
-    name: "theme",
-    message: "What theme for the note?",
-    choices: ["Default"],
+    type: 'list',
+    name: 'theme',
+    message: 'What theme for the note?',
+    choices: ['Default'],
     filter: function (val) {
-      return val.toLowerCase().replace(/\s/g, "").trim();
-    },
+      return val.toLowerCase().replace(/\s/g, '').trim()
+    }
   },
   {
-    type: "confirm",
-    name: "directory",
+    type: 'confirm',
+    name: 'directory',
     message: 'Create "./images" directory?',
-    default: true,
-  },
-];
+    default: true
+  }
+]
 
 inquirer
   .prompt(questions)
   .then((answers) => {
     fs.copyFile(
-      __dirname + "/templates/" + answers.type + ".md",
-      answers.type + ".md",
+      path.join(__dirname, 'templates', answers.type + '.md'),
+      answers.type + '.md',
       (err) => {
-        if (err) throw err;
-        console.log(`Created ${answers.type}.md`);
+        if (err) throw err
+        console.log(`Created ${answers.type}.md`)
       }
-    );
+    )
 
     fs.copyFile(
-      __dirname +
-        "/themes/" +
-        answers.theme +
-        "/" +
-        answers.type +
-        "-style.less",
-      answers.type + "-style.less",
+      path.join(__dirname, 'themes', answers.type + '-style.less'),
+      answers.type + '-style.less',
       (err) => {
-        if (err) throw err;
-        console.log(`Created ${answers.type}-style.less`);
+        if (err) throw err
+        console.log(`Created ${answers.type}-style.less`)
       }
-    );
-    
+    )
+
     if (answers.directory) {
-      fs.mkdir("images", (err) => {
-        if (err != null && err.code != 'EEXIST') throw err;
-        console.log(`Created images directory`);
-      });
+      fs.mkdir('images', (err) => {
+        if (err != null && err.code !== 'EEXIST') throw err
+        console.log('Created images directory')
+      })
     }
   })
   .catch((error) => {
     if (error.isTtyError) {
       // Prompt couldn't be rendered in the current environment
-      console.log("The environment could not be rendered");
+      console.log('The environment could not be rendered')
     } else {
       // Something else when wrong
-      console.log("Unknown error occurred");
+      console.log('Unknown error occurred')
     }
-  });
+  })
